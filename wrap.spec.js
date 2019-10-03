7aabe5b175f97f6fb4b83a8de4f59cad33cda31a
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-03 10:56:59 -0500

    adds tests for wrap function behavior

--- wrap.spec.js
+++ wrap.spec.js
@@ -12,9 +12,33 @@ describe("wrap", () => {
       expect(wrap("",1)).to.be.a("string")
       expect(wrap(1,"")).to.throw(TypeError, "arguments wrong")
   })
-  
   it("returns an empty string if empty string is passed", ()=>{
     expect(wrap("",1)).to.be("")
-  }
-  
+  });
+});
+
+describe("wrap behavior", () => {
+  it("breaks lines if number >= string length", () => {
+    expect(wrap("str ing", 3)).to.be("str\ning");
+    expect(wrap("string", 6)).to.be("string");
+    expect(wrap("string", 7)).to.be("string");
+  });
+  it("doesn't break at the end of the string", () => {
+    expect(wrap("str ing", 3)).to.be("str\ning");
+  });
+  it("inserts as many line breaks as expected", () => {
+    expect(wrap("str ing", 3).lastIndexOf('\n')).to.be(3);
+    expect(wrap("st ri ng", 2).lastIndexOf('\n')).to.be(6);
+    expect(wrap("s t r i n g", 1).lastIndexOf('\n')).to.be(9);
+  });
+  it("length of each `row`", () => {
+    const testString = wrap("alpha bet", 4);
+    const splitString = testString.split('\n');
+    const lastLine = splitString.pop();
+    splitString.forEach(substring => {
+      expect(substring.length).to.be(4)
+    });
+    expect([1,2,3,4]).to.include(lastLine.length);
+  });
+  // TO DO: breaking mid-word (it shouldn't)
 });
